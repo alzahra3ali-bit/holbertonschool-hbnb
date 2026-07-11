a0bda9cee0830ac82a89b36c4d8bf0c6b8edba9b
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-07-11 20:01:18 +0200

    user class 1

---
+++ part2/app/models/user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from app.models import BaseModel
+
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, password, is_admin=False):
+        
+        """Call the attributes from parent class"""
+        super().__init__()
+
+        """Data validation"""
+        if not first_name or len(first_name.strip()) == 0:
+            raise ValueError('First name is required')
+        if not last_name or len(last_name.strip()) == 0:
+            raise ValueError('Last name is required')
+        if not email or '@' not in email:
+            raise ValueError('Invalid email address')
+
+        """Attribute Assignment,
+        .strip() removes accidental leading/trailing spaces from inputs
+        """
+        self.first_name = first_name.strip()
+        self.last_name = last_name.strip()
+        self.email = email.strip()
+        self.is_admin = is_admin
