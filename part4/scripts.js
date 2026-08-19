bb00534d3a5ec5b22c51aa78d302c794a4aa0eb5
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-08-19 16:27:52 +0300

    Show/hide review button based on authentication token
    Add functionality to show/hide review button based on token presence.

--- part4/scripts.js
+++ part4/scripts.js
@@ -1,1 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const token = getCookie('token');
 
+    const addReviewSection = document.getElementById('add-review-button');
+    if (addReviewSection) {
+        if (!token) {
+            addReviewSection.style.display = 'none';
+        } else {
+            addReviewSection.style.display = 'block';
+        }
+    }
+});
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
+}
